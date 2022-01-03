8e608821e958277cbd72d5b1fe6121471a905236
Room9 <444jop@gmail.com>
2022-01-03 16:26:21 +0900

    NCP Server

--- blog/main.tf
+++ blog/main.tf
@@ -230,3 +230,56 @@ data "ncloud_server_images" "server_images" {
     }
   output_file = "ubuntu_server_image_list.json"
 }
+
+data "ncloud_server_product" "product"{
+  server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR1804.B050"
+  filter {
+    name   = "product_code"
+    values = ["SSD"]
+    regex  = true
+  }
+  filter {
+    name   = "cpu_count"
+    values = ["2"]
+  }
+  filter {
+    name   = "memory_size"
+    values = ["4GB"]
+  }
+  filter {
+    name   = "product_type"
+    values = ["HICPU"]
+  }
+}
+output "product" {
+  value = data.ncloud_server_product.product.product_code
+}
+
+## ACG ##
+resource "ncloud_access_control_group" "acg" {
+  name        = "pub-acg"
+  vpc_no      = ncloud_vpc.fe.id
+}
+## ACG Rule ##
+resource "ncloud_access_control_group_rule" "acg-rule" {
+  access_control_group_no = ncloud_access_control_group.acg.id
+  inbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = "22"
+    description = "accept 22 port"
+  }
+}
+## login key ##
+resource "ncloud_login_key" "loginkey" {
+  key_name = "blog-post-key"
+}
+
+## Server ##
+resource "ncloud_server" "server" {
+  subnet_no                 = ncloud_subnet.pubsub.id
+  name                      = "post-server"
+  server_product_code = "SVR.VSVR.HICPU.C002.M004.NET.SSD.B050.G002"
+  server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR1804.B050"
+  login_key_name            = ncloud_login_key.loginkey.key_name
+}
