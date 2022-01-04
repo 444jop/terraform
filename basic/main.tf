f89292506603c5ece564941f48b4ee2ecd1d38cf
Room9 <444jop@gmail.com>
2022-01-04 15:52:24 +0900

    basic infra upload

--- basic/main.tf
+++ basic/main.tf
@@ -17,221 +17,102 @@ provider "ncloud" {
 
 ## NETWORKING ##
 ## VPC ##
-resource "ncloud_vpc" "fe" {
-  name            = "fe-vpc"
+resource "ncloud_vpc" "vpc" {
+  name            = "basic-vpc"
   ipv4_cidr_block = "10.0.0.0/16"
 }
 
-resource "ncloud_vpc" "be" {
-  name            = "be-vpc"
-  ipv4_cidr_block = "10.1.0.0/16"
-}
-
-resource "ncloud_vpc" "mgmt" {
-  name            = "mgmt-vpc"
-  ipv4_cidr_block = "10.2.0.0/16"
-}
-
 ## NACL ##
-resource "ncloud_network_acl" "pubsub" {
-  vpc_no = ncloud_vpc.fe.id
-  name   = "fe-pub-subnet-nacl"
+resource "ncloud_network_acl" "pub-sub" {
+  vpc_no = ncloud_vpc.vpc.id
+  name   = "pub-subnet-nacl"
 }
 
-resource "ncloud_network_acl" "prisub" {
-  vpc_no = ncloud_vpc.be.id
-  name   = "be-pri-subnet-nacl"
-
-}
-resource "ncloud_network_acl" "prisub2" {
-  vpc_no = ncloud_vpc.mgmt.id
-  name   = "mgmt-pri-subnet-nacl"
+resource "ncloud_network_acl" "pri-sub" {
+  vpc_no = ncloud_vpc.vpc.id
+  name   = "pri-subnet-nacl"
 }
 
 ## NACL Rule ##
-resource "ncloud_network_acl_rule" "pubsub" {
-  network_acl_no = ncloud_network_acl.pubsub.id
+resource "ncloud_network_acl_rule" "pub-sub" {
+  network_acl_no = ncloud_network_acl.pub-sub.id
 }
 
-resource "ncloud_network_acl_rule" "prisub" {
-  network_acl_no = ncloud_network_acl.prisub.id
-}
-
-resource "ncloud_network_acl_rule" "prisub2" {
-  network_acl_no = ncloud_network_acl.prisub2.id
+resource "ncloud_network_acl_rule" "pri-sub" {
+  network_acl_no = ncloud_network_acl.pri-sub.id
 }
 
 ## Subnet ##
-resource "ncloud_subnet" "pubsub" {
-  vpc_no         = ncloud_vpc.fe.id
+resource "ncloud_subnet" "pub-sub" {
+  vpc_no         = ncloud_vpc.vpc.id
   subnet         = "10.0.0.0/16"
   zone           = "KR-2"
-  network_acl_no = ncloud_network_acl.pubsub.id
+  network_acl_no = ncloud_network_acl.pub-sub.id
   subnet_type    = "PUBLIC"
   name           = "pub-subnet"
   usage_type     = "GEN"
 }
 
-resource "ncloud_subnet" "prisub" {
-  vpc_no         = ncloud_vpc.be.id
+resource "ncloud_subnet" "pri-sub" {
+  vpc_no         = ncloud_vpc.vpc.id
   subnet         = "10.1.0.0/16"
   zone           = "KR-2"
-  network_acl_no = ncloud_network_acl.prisub.id
+  network_acl_no = ncloud_network_acl.pri-sub.id
   subnet_type    = "PRIVATE"
   name           = "pri-subnet"
   usage_type     = "GEN"
 }
 
-resource "ncloud_subnet" "prisub2" {
-  vpc_no         = ncloud_vpc.mgmt.id
-  subnet         = "10.2.0.0/16"
-  zone           = "KR-2"
-  network_acl_no = ncloud_network_acl.prisub2.id
-  subnet_type    = "PRIVATE"
-  name           = "pri2-subnet"
-  usage_type     = "GEN"
-}
 ## route table ##
-resource "ncloud_route_table" "fe_pub_rt" {
-  vpc_no = ncloud_vpc.fe.id
+resource "ncloud_route_table" "pub-rt" {
+  vpc_no                = ncloud_vpc.vpc.id
   supported_subnet_type = "PUBLIC"
-  name = "fe-pub-rt"
+  name                  = "pub-rt"
 }
-resource "ncloud_route_table" "be_pri_rt" {
-  vpc_no = ncloud_vpc.be.id
+resource "ncloud_route_table" "pri-rt" {
+  vpc_no                = ncloud_vpc.vpc.id
   supported_subnet_type = "PRIVATE"
-  name = "be-pri-rt" 
-}
-resource "ncloud_route_table" "mgmt_pri_rt" {
-  vpc_no = ncloud_vpc.mgmt.id
-  supported_subnet_type = "PRIVATE"
-  name = "mgmt-pri-rt"
+  name                  = "pri-rt"
 }
 
 ## route table - subnet association ##
-resource "ncloud_route_table_association" "fe_rt_sub" {
-  route_table_no = ncloud_route_table.fe_pub_rt.id
-  subnet_no = ncloud_subnet.pubsub.id  
+resource "ncloud_route_table_association" "pub-sub-rt-ass" {
+  route_table_no = ncloud_route_table.pub-rt.id
+  subnet_no      = ncloud_subnet.pub-sub.id
 }
 
-resource "ncloud_route_table_association" "be_rt_sub" {
-  route_table_no = ncloud_route_table.be_pri_rt.id
-  subnet_no = ncloud_subnet.prisub.id
-}
-
-resource "ncloud_route_table_association" "mgmt_rt_sub" {
-  route_table_no = ncloud_route_table.mgmt_pri_rt.id
-  subnet_no = ncloud_subnet.prisub2.id
+resource "ncloud_route_table_association" "pri-sub-rt-ass" {
+  route_table_no = ncloud_route_table.pri-rt.id
+  subnet_no      = ncloud_subnet.pri-sub.id
 }
 
 ## NAT Gateway ##
 resource "ncloud_nat_gateway" "nat_gateway" {
-  vpc_no = ncloud_vpc.be.id
+  vpc_no = ncloud_vpc.vpc.id
   zone   = "KR-2"
   name   = "nat-gw"
 }
 
 ## Route ##
 resource "ncloud_route" "natroute" {
-  route_table_no = ncloud_route_table.be_pri_rt.id
+  route_table_no         = ncloud_route_table.pri-rt.id
   destination_cidr_block = "0.0.0.0/0"
-  target_type = "NATGW"
-  target_name = ncloud_nat_gateway.nat_gateway.name
-  target_no = ncloud_nat_gateway.nat_gateway.id
+  target_type            = "NATGW"
+  target_name            = ncloud_nat_gateway.nat_gateway.name
+  target_no              = ncloud_nat_gateway.nat_gateway.id
 }
-
-## vpc peering 라우팅 필요
-resource "ncloud_route" "peering_route" {
-  route_table_no = ncloud_route_table.fe_pub_rt.id
-  destination_cidr_block = "10.1.0.0/16"
-  target_type = "VPCPEERING"
-  target_name = ncloud_vpc_peering.peering.name
-  target_no = ncloud_vpc_peering.peering.id
-}
-
-resource "ncloud_route" "peering2_route" {
-  route_table_no = ncloud_route_table.be_pri_rt.id
-  destination_cidr_block = "10.0.0.0/16"
-  target_type = "VPCPEERING"
-  target_name = ncloud_vpc_peering.peering2.name
-  target_no = ncloud_vpc_peering.peering2.id
-}
-resource "ncloud_route" "peering3_route" {
-  route_table_no = ncloud_route_table.be_pri_rt.id
-  destination_cidr_block = "10.2.0.0/16"
-  target_type = "VPCPEERING"
-  target_name = ncloud_vpc_peering.peering3.name
-  target_no = ncloud_vpc_peering.peering3.id
-}
-
-resource "ncloud_route" "peering4_route" {
-  route_table_no = ncloud_route_table.mgmt_pri_rt.id
-  destination_cidr_block = "10.1.0.0/16"
-  target_type = "VPCPEERING"
-  target_name = ncloud_vpc_peering.peering4.name
-  target_no = ncloud_vpc_peering.peering4.id
-}
-
-resource "ncloud_route" "peering5_route" {
-  route_table_no = ncloud_route_table.fe_pub_rt.id
-  destination_cidr_block = "10.2.0.0/16"
-  target_type = "VPCPEERING"
-  target_name = ncloud_vpc_peering.peering5.name
-  target_no = ncloud_vpc_peering.peering5.id
-}
-
-resource "ncloud_route" "peering6_route" {
-  route_table_no = ncloud_route_table.mgmt_pri_rt.id
-  destination_cidr_block = "10.0.0.0/16"
-  target_type = "VPCPEERING"
-  target_name = ncloud_vpc_peering.peering6.name
-  target_no = ncloud_vpc_peering.peering6.id
-}
-
-## VPC Peering ##
-resource "ncloud_vpc_peering" "peering" {
-  name          = "vpc-peering"
-  source_vpc_no = ncloud_vpc.fe.id
-  target_vpc_no = ncloud_vpc.be.id
-}
-resource "ncloud_vpc_peering" "peering2" {
-  name          = "vpc-peering2"
-  source_vpc_no = ncloud_vpc.be.id
-  target_vpc_no = ncloud_vpc.fe.id
-}
-resource "ncloud_vpc_peering" "peering3" {
-  name          = "vpc-peering3"
-  source_vpc_no = ncloud_vpc.be.id
-  target_vpc_no = ncloud_vpc.mgmt.id
-}
-resource "ncloud_vpc_peering" "peering4" {
-  name          = "vpc-peering4"
-  source_vpc_no = ncloud_vpc.mgmt.id
-  target_vpc_no = ncloud_vpc.be.id
-}
-resource "ncloud_vpc_peering" "peering5" {
-  name          = "vpc-peering5"
-  source_vpc_no = ncloud_vpc.fe.id
-  target_vpc_no = ncloud_vpc.mgmt.id
-}
-resource "ncloud_vpc_peering" "peering6" {
-  name          = "vpc-peering6"
-  source_vpc_no = ncloud_vpc.mgmt.id
-  target_vpc_no = ncloud_vpc.fe.id
-}
-
 ## server ##
 # ubuntu 서버 이미지 리스트 #
 data "ncloud_server_images" "server_images" {
-    filter {
-      name = "product_name"
-      values = ["ubuntu"]
-      regex = true
-    }
+  filter {
+    name   = "product_name"
+    values = ["ubuntu"]
+    regex  = true
+  }
   output_file = "ubuntu_server_image_list.json"
 }
 
-data "ncloud_server_product" "product"{
+data "ncloud_server_product" "product" {
   server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR1804.B050"
   filter {
     name   = "product_code"
@@ -257,8 +138,8 @@ output "product" {
 
 ## ACG ##
 resource "ncloud_access_control_group" "acg" {
-  name        = "pub-acg"
-  vpc_no      = ncloud_vpc.fe.id
+  name   = "pub-acg"
+  vpc_no = ncloud_vpc.fe.id
 }
 ## ACG Rule ##
 resource "ncloud_access_control_group_rule" "acg-rule" {
@@ -279,7 +160,7 @@ resource "ncloud_login_key" "loginkey" {
 resource "ncloud_server" "server" {
   subnet_no                 = ncloud_subnet.pubsub.id
   name                      = "post-server"
-  server_product_code = "SVR.VSVR.HICPU.C002.M004.NET.SSD.B050.G002"
+  server_product_code       = "SVR.VSVR.HICPU.C002.M004.NET.SSD.B050.G002"
   server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR1804.B050"
   login_key_name            = ncloud_login_key.loginkey.key_name
 }
